e3c4d8eef9dff50f5a0cd57e9114bf3242e02add
Aaron Hertner <aaron36hertner@gmail.com>
2019-05-16 16:34:55 -0400

    Added golfer class

---
+++ src/Golfer.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Golfer {
+    public String  name;
+    public int     age;
+    private int    score;
+
+    public Golfer(){
+        score = 0;
+    }
+
+    public void setScore(int x){
+        this.score = x;
+    }
+
+    public void incScore(int x){
+        Scanner input = new Scanner(System.in);
+        System.out.println("What was your score from the last hole? ");
+        this.setScore(this.score + input.nextInt());
+        System.out.println("Your current score is: " + this.score);
+    }
+
+}
